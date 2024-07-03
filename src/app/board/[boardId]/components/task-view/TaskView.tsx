cd5c64fe09ceaa5cf6917d421dfcb8e7b0ac8efe
EdiedRamos <edieisa22@gmail.com>
2024-07-03 01:17:08 -0500

    feat: add task view provider

--- src/app/board/[boardId]/components/task-view/TaskView.tsx
+++ src/app/board/[boardId]/components/task-view/TaskView.tsx
@@ -5,6 +5,7 @@ import { TaskViewControl } from "./TaskViewControl";
 import { TaskViewDescription } from "./TaskViewDescription";
 import { TaskViewIcons } from "./TaskViewIcons";
 import { TaskViewName } from "./TaskViewName";
+import { TaskViewProvider } from "../../provider";
 import { TaskViewStatus } from "./TaskViewStatus";
 import { useBoard } from "../../hook/useBoard";
 
@@ -14,25 +15,27 @@ export const TaskView = () => {
   if (!boardState.showTaskDetails) return <></>;
 
   return (
-    <div className="fixed flex justify-end bg-cc-gray left-0 top-0 w-full min-h-screen">
-      <div className="bg-cc-white w-full sm:w-[558px] m-3 rounded-lg p-3 flex flex-col overflow-y-auto max-h-screen">
-        <div className="flex justify-between">
-          <p className="font-medium text-cf-300">Task details</p>
-          <button
-            onClick={boardState.handleTaskViewClose}
-            className="border p-1 rounded-lg active:scale-105 transition-all hover:bg-slate-100"
-          >
-            <Close />
-          </button>
-        </div>
-        <div>
-          <TaskViewName />
-          <TaskViewDescription />
-          <TaskViewIcons />
-          <TaskViewStatus />
-          <TaskViewControl />
+    <TaskViewProvider>
+      <div className="fixed flex justify-end bg-cc-gray left-0 top-0 w-full min-h-screen">
+        <div className="bg-cc-white w-full sm:w-[558px] m-3 rounded-lg p-3 flex flex-col overflow-y-auto max-h-screen">
+          <div className="flex justify-between">
+            <p className="font-medium text-cf-300">Task details</p>
+            <button
+              onClick={boardState.handleTaskViewClose}
+              className="border p-1 rounded-lg active:scale-105 transition-all hover:bg-slate-100"
+            >
+              <Close />
+            </button>
+          </div>
+          <div>
+            <TaskViewName />
+            <TaskViewDescription />
+            <TaskViewIcons />
+            <TaskViewStatus />
+            <TaskViewControl />
+          </div>
         </div>
       </div>
-    </div>
+    </TaskViewProvider>
   );
 };
