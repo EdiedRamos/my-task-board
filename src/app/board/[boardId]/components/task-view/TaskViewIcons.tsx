a22bd111e8dda3c85b01348b28a74d0c8e0b648e
EdiedRamos <edieisa22@gmail.com>
2024-07-03 01:32:04 -0500

    feat: connect current icon in task view icons

--- src/app/board/[boardId]/components/task-view/TaskViewIcons.tsx
+++ src/app/board/[boardId]/components/task-view/TaskViewIcons.tsx
@@ -3,13 +3,22 @@
 import { IconButton, Label } from "../atoms";
 import { Icons, IconsKeys } from "../../assets";
 
+import { useTaskView } from "../../hook";
+
 export const TaskViewIcons = () => {
+  const { handleClickIcon, iconName } = useTaskView();
+
   return (
     <div className="mt-5">
       <Label text="Icon" />
       <div className="flex gap-3 flex-wrap mt-2">
         {IconsKeys.map((name) => (
-          <IconButton key={name} icon={Icons[name]({ className: "w-5" })} />
+          <IconButton
+            isActive={name === iconName}
+            key={name}
+            icon={Icons[name]({ className: "w-5" })}
+            onClick={() => handleClickIcon(name)}
+          />
         ))}
       </div>
     </div>
